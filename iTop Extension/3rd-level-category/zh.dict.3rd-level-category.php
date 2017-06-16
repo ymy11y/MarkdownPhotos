3723c9bb6ad7e0c9fe0a98f06cfa19fdb3e2106b
ymy11y <ymy11yu@163.com>
2017-06-16 09:59:39 +0800

    6/15: Add service3rdlevelcategory to Service, meanwhile display the added field to Request when resolving state

---
+++ iTop Extension/3rd-level-category/zh.dict.3rd-level-category.php
@@ -0,0 +1,44 @@
+<?php
+// Copyright (C) 2010-2014 Combodo SARL
+//
+//   This file is part of iTop.
+//
+//   iTop is free software; you can redistribute it and/or modify	
+//   it under the terms of the GNU Affero General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   iTop is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU Affero General Public License for more details.
+//
+//   You should have received a copy of the GNU Affero General Public License
+//   along with iTop. If not, see <http://www.gnu.org/licenses/>
+
+
+/**
+ * Localized data
+ * @Maxima	maximaqiu@hotmail.com
+ * @copyright   Copyright (C) 2010-2012 Combodo SARL
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+ // Class: service3rdlevelcategory
+ 
+Dict::Add('ZH CN', 'Chinese', '简体中文', array(
+	'Menu:service3rdlevelcategory' => '解决原因',
+	'Menu:service3rdlevelcategory+' => '解决原因',
+	'Class:service3rdlevelcategory' => '解决原因',
+	'Class:service3rdlevelcategory/Attribute:name' => '名称',
+	'Class:service3rdlevelcategory/Attribute:name+' => '',
+	'Class:service3rdlevelcategory/Attribute:servicesubcategory_id' => '所属二级类目',
+	'Class:service3rdlevelcategory/Attribute:servicesubcategory_id+' => '',
+	'Class:service3rdlevelcategory/Attribute:description' => '描述',
+	'Class:service3rdlevelcategory/Attribute:description+' => '',
+	'Class:service3rdlevelcategory/Attribute:status' => '状态',
+	'Class:service3rdlevelcategory/Attribute:status+' => '',
+	
+	
+));
+
