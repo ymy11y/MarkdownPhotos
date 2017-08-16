e9dcfc88c5e0013607b0ba15287f227f86dacf60
ymy11y <ymy11yu@163.com>
2017-08-16 19:27:59 +0800

    20170816 增加事件和问题的字段并

---
+++ iTop Extension/20170803 Request增加关闭审核人/zh.dict.msxf-add-productcode.php
@@ -0,0 +1,50 @@
+<?php
+// Copyright (C) 2010-2014 Combodo SARL
+//
+//   This file is part of iTop.
+//
+//   iTop is free software; you can redistribute it and/or modify	
+//   it under the terms of the GNU Affero General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   iTop is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU Affero General Public License for more details.
+//
+//   You should have received a copy of the GNU Affero General Public License
+//   along with iTop. If not, see <http://www.gnu.org/licenses/>
+
+
+/**
+ * Localized data
+ * @Maxima	maximaqiu@hotmail.com
+ * @copyright   Copyright (C) 2010-2012 Combodo SARL
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+ // Class: UserRequest
+ 
+Dict::Add('ZH CN', 'Chinese', '简体中文', array(
+
+	'Class:UserRequest/Attribute:service3rdlevelcategory_id' => '解决原因',
+	'Class:UserRequest/Attribute:service3rdlevelcategory_id+' => '',
+    'Class:UserRequest/Attribute:resolution_date' => '解决日期',
+    'Class:UserRequest/Attribute:resolution_date+' => '',
+	'Class:UserRequest/Attribute:prodcode' => '产品编码',
+    'Class:UserRequest/Attribute:prodcode+' => '',
+	'Class:UserRequest/Attribute:escalation_reason+' => '  二类卡
+  银行到账慢
+  放款标识
+  日终退回
+  查询无结果
+  信息不全
+  溢交款
+  线下还款
+  余额不足',
+	'Class:UserRequest/Attribute:validator_id' => '关闭审批者',
+    'Class:UserRequest/Attribute:validator_id+' => '',
+	
+));
+
